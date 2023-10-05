a0f202b7eff1b952c7bab88849bd23fb3fee7aa4
JoseManzi <106353210+JoseManzi@users.noreply.github.com>
2023-10-05 20:54:16 -0300

    Modificación sección libros (#17)
    * Modificacion Libros, agregado de img y estetica QS
    
    * Mod Libros

--- js/card.js
+++ js/card.js
@@ -1,13 +1,16 @@
 var modal = document.getElementById("myModal");
 
-var img = document.getElementById("myImg");
+var images = document.querySelectorAll("#myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+
+Array.prototype.forEach.call(images, function(img) {
+  img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+});
 
 var span = document.getElementsByClassName("close")[0];
 
