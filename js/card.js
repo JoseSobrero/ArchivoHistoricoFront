a76bf45fa0b9bc205b764743c3eb96212dd38a05
Juan-Audisio <juanmaudisio@gmail.com>
2023-10-18 19:09:26 -0300

    modificacion scroll carrusel

--- js/card.js
+++ js/card.js
@@ -17,3 +17,19 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function () {
   modal.style.display = "none";
 }
+
+// const track = document.querySelector('.carousel-track');
+// const items = document.querySelectorAll('.carousel-item');
+// let index = 0;
+
+// function nextSlide() {
+//   index = (index + 1) % items.length;
+//   updateCarousel();
+// }
+
+// function updateCarousel() {
+//   const offset = -index * 100 + '%';
+//   track.style.transform = 'translateX(' + offset + ')';
+// }
+
+// setInterval(nextSlide, 10000); // Cambia al siguiente slide cada 10 segundos
